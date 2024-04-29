64a7387fdc1ce144b2ddc91590189136c86049e5
anant-jain01 <139585700+anant-jain01@users.noreply.github.com>
2024-04-29 20:52:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const display = calculator.querySelector('.calculator__display')
+
+keys.addEventListener('click', event => {
+  if (!event.target.closest('button')) return
+
+  const key = event.target
+  const keyValue = key.textContent
+  const displayValue = display.textContent
+  const { type } = key.dataset
+  const { previousKeyType } = calculator.dataset
+
+  if (type === 'number') {
+    if (
+      displayValue === '0' ||
+      previousKeyType === 'operator'
+    ) {
+      display.textContent = keyValue
+    } else {
+      display.textContent = displayValue + keyValue
+    }
+  }
+
+  if (type === 'operator') {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
+    operatorKeys.forEach(el => { el.dataset.state = '' })
+    key.dataset.state = 'selected'
+
+    calculator.dataset.firstNumber = displayValue
+    calculator.dataset.operator = key.dataset.key
+  }
+  if (type === 'clear') {
+    calculator.dataset.firstNumber = '0'
+    display.textContent = '0'
+    calculator.dataset.operator=null 
+}
+
+  if (type === 'equal') {
+    // Perform a calculation
+    const firstNumber = calculator.dataset.firstNumber
+    const operator = calculator.dataset.operator
+    const secondNumber = displayValue
+    display.textContent = calculate(firstNumber, operator, secondNumber)
+  }
+
+  calculator.dataset.previousKeyType = type
+})
+
+function calculate (firstNumber, operator, secondNumber) {
+  firstNumber = parseFloat(firstNumber)
+  secondNumber = parseFloat(secondNumber)
+
+  if (operator === 'plus') return firstNumber + secondNumber
+  if (operator === 'minus') return firstNumber - secondNumber
+  if (operator === 'times') return firstNumber * secondNumber
+  if (operator === 'divide') return firstNumber / secondNumber
+}
